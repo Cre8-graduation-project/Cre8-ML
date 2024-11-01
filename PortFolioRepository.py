69899b2d1d0c465effd6038086142bf52302d643
Jinu Yi <dionisos198@naver.com>
2024-11-01 14:28:05 +0900

    Refactor: portfolio select

--- PortFolioRepository.py
+++ PortFolioRepository.py
@@ -5,29 +5,35 @@ import os
 
 load_dotenv()
 
-db = mysql.connector.connect(
-    host=os.getenv("MYSQL_host"),                  # MySQL 서버 호스트 (Spring 설정의 'localhost')
-    user=os.getenv("MYSQL_USER"),                         # MySQL 사용자 이름 (Spring 설정의 'root')
-    password=os.getenv("MYSQL_PASSWORD"),                      # MySQL 비밀번호 (Spring 설정의 'root')
-    database=os.getenv("MYSQL_DATABASE"),           # 사용할 데이터베이스 (Spring 설정의 'cre8_local')
-    charset="utf8"                        # 문자 인코딩 설정
-)
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST"),                  # MySQL 서버 호스트
+        user=os.getenv("MYSQL_USER"),                  # MySQL 사용자 이름
+        password=os.getenv("MYSQL_PASSWORD"),          # MySQL 비밀번호
+        database=os.getenv("MYSQL_DATABASE"),          # 사용할 데이터베이스
+        charset="utf8"                                 # 문자 인코딩 설정
+    )
 
 def get_portfolio():
-    # 커서 객체 생성
+    try:
+        # 데이터베이스 커넥션 생성 및 커서 객체 생성
+        db = get_db_connection()
+        cursor = db.cursor()
 
-    cursor = db.cursor()
+        # SQL 쿼리 실행
+        cursor.execute("SELECT portfolio_id, access_url FROM portfolio_image")
 
-    # SQL 쿼리 실행
-    cursor.execute("SELECT portfolio_id, access_url FROM portfolio_image")
+        # 쿼리 결과 가져오기
+        results = cursor.fetchall()
+        return results
 
-    # 쿼리 결과 가져오기
-    results = cursor.fetchall()
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
 
-    print(results)
-
-    # 커서 및 연결 닫기
-    cursor.close()
-
-
-    return results
+    finally:
+        # 커서 및 연결 닫기
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
